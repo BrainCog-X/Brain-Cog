05668fc07e9371ac9d6d346311afa0b71efa6d69
TianlongLee <1033645698@qq.com>
2022-07-30 21:38:00 +0800

    refactor: Modified __init__ file for importing directly
    修改了__init__文件使得所有需要重复import相同文件名的功能，如可直接import braincog.base.node.IFNode 而无需两次书写node

--- braincog/base/brainarea/__init__.py
+++ braincog/base/brainarea/__init__.py
@@ -1,8 +1,8 @@
 from .basalganglia import basalganglia
 from .BrainArea import BrainArea, ThreePointForward, Feedback, TwoInOneOut, SelfConnectionArea
-from Insula import InsulaNet
-from IPL import IPLNet
-from PFC import PFC, dlPFC
+from .Insula import InsulaNet
+from .IPL import IPLNet
+from .PFC import PFC, dlPFC
 
 
 __all__ = [
