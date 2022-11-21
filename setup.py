c416b51ac84f88bd75842cecdd10d9489adaa0a7
sunbaby01 <dongyiting01@126.com>
2022-11-21 05:58:37 +0000

    setup

--- setup.py
+++ setup.py
@@ -3,10 +3,14 @@ from setuptools import setup
 
 with open("./requirements.txt", "r", encoding="utf-8") as fh:
     install_requires = fh.read()
-
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 setup(
     install_requires=install_requires,
     packages=find_packages(),
+    description="BrainCog is an open source spiking neural network based brain-inspired cognitive intelligence engine for Brain-inspired Artificial Intelligence and brain simulation. More information on braincog can be found on its homepage http://www.brain-cog.network/",
+    long_description=long_description,  
+    long_description_content_type="text/markdown",  
     classifiers=[
         "Programming Language :: Python :: 3 :: Only",
         "Programming Language :: Python :: 3.6",
@@ -18,6 +22,6 @@ setup(
     ],
     name='braincog',
     version='0.2.7.12',
-    author='',
+    author='braincog',
     python_requires='>=3.6'
 )
