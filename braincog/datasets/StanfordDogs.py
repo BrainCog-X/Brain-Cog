fcdac5a4dde29e2d196a54ab769b6d4ea278d28e
sunbaby <45324085+sunbaby01@users.noreply.github.com>
2022-12-03 17:05:37 +0800

    Add files via upload

---
+++ braincog/datasets/StanfordDogs.py
@@ -0,0 +1,105 @@
+import os
+import scipy.io
+from os.path import join
+from torchvision.datasets import VisionDataset
+from torchvision.datasets.folder import default_loader
+from torchvision.datasets.utils import download_url, list_dir
+
+
+class StanfordDogs(VisionDataset):
+    """`Stanford Dogs <http://vision.stanford.edu/aditya86/ImageNetDogs/>`_ Dataset.
+        Args:
+            root (string): Root directory of the dataset.
+            train (bool, optional): If True, creates dataset from training set, otherwise
+               creates from test set.
+            transform (callable, optional): A function/transform that  takes in an PIL image
+               and returns a transformed version. E.g, ``transforms.RandomCrop``
+            target_transform (callable, optional): A function/transform that takes in the
+               target and transforms it.
+            download (bool, optional): If true, downloads the dataset from the internet and
+               puts it in root directory. If dataset is already downloaded, it is not
+               downloaded again.
+    """
+    download_url_prefix = 'http://vision.stanford.edu/aditya86/ImageNetDogs'
+
+    def __init__(self, root, train=True, transform=None, target_transform=None, download=False):
+        super(StanfordDogs, self).__init__(root, transform=transform, target_transform=target_transform)
+
+        self.loader = default_loader
+        self.train = train
+
+        if download:
+            self.download()
+
+        split = self.load_split()
+
+        self.images_folder = join(self.root, 'Images')
+        self.annotations_folder = join(self.root, 'Annotation')
+        self._breeds = list_dir(self.images_folder)
+
+        self._breed_images = [(annotation + '.jpg', idx) for annotation, idx in split]
+
+        self._flat_breed_images = self._breed_images
+
+    def __len__(self):
+        return len(self._flat_breed_images)
+
+    def __getitem__(self, index):
+        image_name, target = self._flat_breed_images[index]
+        image_path = join(self.images_folder, image_name)
+        image = self.loader(image_path)
+
+        if self.transform is not None:
+            image = self.transform(image)
+        if self.target_transform is not None:
+            target = self.target_transform(target)
+        return image, target
+
+    def download(self):
+        import tarfile
+
+        if os.path.exists(join(self.root, 'Images')) and os.path.exists(join(self.root, 'Annotation')):
+            if len(os.listdir(join(self.root, 'Images'))) == len(os.listdir(join(self.root, 'Annotation'))) == 120:
+                print('Files already downloaded and verified')
+                return
+
+        for filename in ['images', 'annotation', 'lists']:
+            tar_filename = filename + '.tar'
+            url = self.download_url_prefix + '/' + tar_filename
+            download_url(url, self.root, tar_filename, None)
+            print('Extracting downloaded file: ' + join(self.root, tar_filename))
+            with tarfile.open(join(self.root, tar_filename), 'r') as tar_file:
+                tar_file.extractall(self.root)
+            os.remove(join(self.root, tar_filename))
+
+    def load_split(self):
+        if self.train:
+            split = scipy.io.loadmat(join(self.root, 'train_list.mat'))['annotation_list']
+            labels = scipy.io.loadmat(join(self.root, 'train_list.mat'))['labels']
+        else:
+            split = scipy.io.loadmat(join(self.root, 'test_list.mat'))['annotation_list']
+            labels = scipy.io.loadmat(join(self.root, 'test_list.mat'))['labels']
+
+        split = [item[0][0] for item in split]
+        labels = [item[0] - 1 for item in labels]
+        return list(zip(split, labels))
+
+    def stats(self):
+        counts = {}
+        for index in range(len(self._flat_breed_images)):
+            image_name, target_class = self._flat_breed_images[index]
+            if target_class not in counts.keys():
+                counts[target_class] = 1
+            else:
+                counts[target_class] += 1
+
+        print("%d samples spanning %d classes (avg %f per class)" % (len(self._flat_breed_images), len(counts.keys()),
+                                                                     float(len(self._flat_breed_images)) / float(
+                                                                         len(counts.keys()))))
+
+        return counts
+
+
+if __name__ == '__main__':
+    train_dataset = Dogs('./dogs', train=True, download=False)
+    test_dataset = Dogs('./dogs', train=False, download=False)
---
+++ braincog/datasets/TinyImageNet.py
@@ -0,0 +1,119 @@
+import os
+import os
+import pandas as pd
+import warnings
+from torchvision.datasets import ImageFolder
+from torchvision.datasets import VisionDataset
+from torchvision.datasets.folder import default_loader
+from torchvision.datasets.folder import default_loader
+from torchvision.datasets.utils import extract_archive, check_integrity, download_url, verify_str_arg
+
+
+class TinyImageNet(VisionDataset):
+    """`tiny-imageNet <http://cs231n.stanford.edu/tiny-imagenet-200.zip>`_ Dataset.
+        Args:
+            root (string): Root directory of the dataset.
+            split (string, optional): The dataset split, supports ``train``, or ``val``.
+            transform (callable, optional): A function/transform that  takes in an PIL image
+               and returns a transformed version. E.g, ``transforms.RandomCrop``
+            target_transform (callable, optional): A function/transform that takes in the
+               target and transforms it.
+            download (bool, optional): If true, downloads the dataset from the internet and
+               puts it in root directory. If dataset is already downloaded, it is not
+               downloaded again.
+    """
+    base_folder = 'tiny-imagenet-200/'
+    url = 'http://cs231n.stanford.edu/tiny-imagenet-200.zip'
+    filename = 'tiny-imagenet-200.zip'
+    md5 = '90528d7ca1a48142e341f4ef8d21d0de'
+
+    def __init__(self, root, split='train', transform=None, target_transform=None, download=False):
+        super(TinyImageNet, self).__init__(root, transform=transform, target_transform=target_transform)
+
+        self.dataset_path = os.path.join(root, self.base_folder)
+        self.loader = default_loader
+        self.split = verify_str_arg(split, "split", ("train", "val",))
+
+        if self._check_integrity():
+            print('Files already downloaded and verified.')
+        elif download:
+            self._download()
+        else:
+            raise RuntimeError(
+                'Dataset not found. You can use download=True to download it.')
+        if not os.path.isdir(self.dataset_path):
+            print('Extracting...')
+            extract_archive(os.path.join(root, self.filename))
+
+        _, class_to_idx = find_classes(os.path.join(self.dataset_path, 'wnids.txt'))
+
+        self.data = make_dataset(self.root, self.base_folder, self.split, class_to_idx)
+
+    def _download(self):
+        print('Downloading...')
+        download_url(self.url, root=self.root, filename=self.filename)
+        print('Extracting...')
+        extract_archive(os.path.join(self.root, self.filename))
+
+    def _check_integrity(self):
+        return check_integrity(os.path.join(self.root, self.filename), self.md5)
+
+    def __getitem__(self, index):
+        img_path, target = self.data[index]
+        image = self.loader(img_path)
+
+        if self.transform is not None:
+            image = self.transform(image)
+        if self.target_transform is not None:
+            target = self.target_transform(target)
+
+        return image, target
+
+    def __len__(self):
+        return len(self.data)
+
+
+def find_classes(class_file):
+    with open(class_file) as r:
+        classes = list(map(lambda s: s.strip(), r.readlines()))
+
+    classes.sort()
+    class_to_idx = {classes[i]: i for i in range(len(classes))}
+
+    return classes, class_to_idx
+
+
+def make_dataset(root, base_folder, dirname, class_to_idx):
+    images = []
+    dir_path = os.path.join(root, base_folder, dirname)
+
+    if dirname == 'train':
+        for fname in sorted(os.listdir(dir_path)):
+            cls_fpath = os.path.join(dir_path, fname)
+            if os.path.isdir(cls_fpath):
+                cls_imgs_path = os.path.join(cls_fpath, 'images')
+                for imgname in sorted(os.listdir(cls_imgs_path)):
+                    path = os.path.join(cls_imgs_path, imgname)
+                    item = (path, class_to_idx[fname])
+                    images.append(item)
+    else:
+        imgs_path = os.path.join(dir_path, 'images')
+        imgs_annotations = os.path.join(dir_path, 'val_annotations.txt')
+
+        with open(imgs_annotations) as r:
+            data_info = map(lambda s: s.split('\t'), r.readlines())
+
+        cls_map = {line_data[0]: line_data[1] for line_data in data_info}
+
+        for imgname in sorted(os.listdir(imgs_path)):
+            path = os.path.join(imgs_path, imgname)
+            item = (path, class_to_idx[cls_map[imgname]])
+            images.append(item)
+
+    return images
+
+
+if __name__ == '__main__':
+    train_dataset = TinyImageNet('./tiny-imagenet', split='train', download=False)
+    test_dataset = TinyImageNet('./tiny-imagenet', split='val', download=False)
+ 
