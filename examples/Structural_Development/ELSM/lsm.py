24b3b36ba449842b093a4affeefc7b0a80ac174f
Agnes <34760282+Agnes233@users.noreply.github.com>
2023-08-08 22:52:12 +0800

    Update lsm.py

--- examples/Structural_Development/ELSM/lsm.py
+++ examples/Structural_Development/ELSM/lsm.py
@@ -14,6 +14,7 @@ import logging
 from timm.utils import *
 from timm.loss import LabelSmoothingCrossEntropy
 from braincog.base.utils import UnilateralMse, MixLoss
+from braincog.base.learningrule.STDP import *
 
 device='cuda:7'
 
